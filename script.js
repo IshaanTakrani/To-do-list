8d1c2745f3f37d3fd0178061e7d5254d080c08f4
IshaanTakrani <ishaantakrani@gmail.com>
2024-01-10 05:12:32 -0500

    Added styles, changed functionality

--- script.js
+++ script.js
@@ -1,84 +1,65 @@
-// const body = document.body; // This is the body of the webpage
-
-// body.appendChild(); // can only append elements (div, span, anchor)
-// body.append("hello world"); // append everything (strings too) This is better
-
-// how to make element?
-
-// const div = document.createElement("div");
-
-// div.innerHTML = "<h1>Hello, I'm div</h1>";
-
-// console.log(div.innerText);      // inner text looks at what is visible on the screen
-// console.log(div.textContent);    // text content is the exact text content
-
-// body.append(div);
-
-// div.setAttribute("class", "big-text"); // change attribute, class, id, anything in that section
-
-// const subheading = document.querySelector("#subheading");
-
-// console.log(subheading.getAttribute("id"));
-
-// subheading.classList.add("new-class"); // changes class
-// subheading.classList.toggle("new-class", false); // if the class doesn't exist, it is added
-// subheading.classList.toggle("new-class-2", true);
-
-// div.style.color = "blue"; // change styles
-
 let input = document.querySelector(".input");
 const addBtn = document.querySelector(".add");
 const items = document.querySelector(".todo-container");
-// const removeBtn = document.querySelector(".remove");
 
-addBtn.addEventListener("click", () => {
+function changeToStrikethrough(item) {
+  if (
+    item.querySelector(".item-text").style.textDecoration === "line-through"
+  ) {
+    item.querySelector(".item-text").style.textDecoration = "none";
+    item.querySelector(".item-text").style.opacity = "1";
+    console.log("click!");
+  } else {
+    item.querySelector(".item-text").style.textDecoration = "line-through";
+    item.querySelector(".item-text").style.opacity = "0.65";
+  }
+}
+
+function addItem() {
   let item = document.createElement("div");
   item.classList.add("item-div");
   let removeBtn = document.createElement("button");
+  let strikeBtn = document.createElement("button");
   let itemText = document.createElement("p");
+  let buttonDiv = document.createElement("div");
 
-  removeBtn.innerHTML = "x";
+  removeBtn.innerHTML =
+    '<img src="media/close_FILL0_wght400_GRAD0_opsz24.svg" alt="close_icon" />';
   removeBtn.classList.add("remove");
 
-  itemText.innerHTML = input.value;
-  itemText.classList.add("item-text");
+  strikeBtn.innerHTML =
+    '<img src="media/done_FILL0_wght400_GRAD0_opsz24.svg" alt="check_icon" />';
+  strikeBtn.classList.add("strike-btn");
 
-  item.innerHTML = itemText.outerHTML + removeBtn.outerHTML;
+  buttonDiv.innerHTML = strikeBtn.outerHTML + removeBtn.outerHTML;
+  buttonDiv.classList.add("button-div");
+
+  itemText.innerHTML = input.value;
+  itemText.classList.toggle("item-text", true);
+
+  item.innerHTML = itemText.outerHTML + buttonDiv.outerHTML;
 
   items.appendChild(item);
   input.value = "";
   input.focus();
 
-  //TODO: add strikethrough when text (not button) is clicked
+  item.querySelector(".item-text").addEventListener("click", () => {
+    changeToStrikethrough(item);
+  });
 
-  item.querySelector(".remove").addEventListener("click", function () {
-    console.log("CLICK!!");
+  item.querySelector(".strike-btn").addEventListener("click", () => {
+    changeToStrikethrough(item);
+  });
+
+  item.querySelector(".remove").addEventListener("click", () => {
     items.removeChild(item);
   });
-});
+}
+
+addBtn.addEventListener("click", addItem);
 
 input.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    let item = document.createElement("div");
-    item.classList.add("item-div");
-    let removeBtn = document.createElement("button");
-    let itemText = document.createElement("p");
-
-    removeBtn.innerHTML = "x";
-    removeBtn.classList.add("remove");
-
-    itemText.innerHTML = input.value;
-    itemText.classList.add("item-text");
-
-    item.innerHTML = itemText.outerHTML + removeBtn.outerHTML;
-
-    items.appendChild(item);
-    input.value = "";
-    input.focus();
-
-    item.querySelector(".remove").addEventListener("click", function () {
-      console.log("CLICK!!");
-      items.removeChild(item);
-    });
+    addItem();
   }
 });
